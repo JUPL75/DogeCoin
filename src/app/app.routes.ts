b349e983087e9a8a91d793dbb2fc21b0ea5e4054
Julie PL <julie.planque75@gmail.com>
2026-02-15 20:15:35 +0100

    Init first feature : crypto dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+        { path: '', redirectTo: 'crypto-dashboard', pathMatch: 'full'},
+        { path: 'crypto-dashboard', loadComponent: () => import('./features/crypto-dashboard/crypto-dashboard.component').then(m => m.CryptoDashboardComponent) },
+];
---
+++ src/app/features/crypto-dashboard/crypto-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-dashboard',
+  imports: [],
+  templateUrl: './crypto-dashboard.component.html',
+  styleUrl: './crypto-dashboard.component.scss'
+})
+export class CryptoDashboardComponent {
+
+}
